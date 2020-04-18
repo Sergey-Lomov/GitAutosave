89d94a3845600ab708866ca7b424e7d59e249f9f
Sergey Lomov <SSpirit10000@gmail.com>
2020-04-18 18:20:26 +0300

    Update version in config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'gas',
-    version = '1.0',
+    version = '0.1.0',
     packages = ['gas','gas.utils','gas.common'],
     entry_points = {
         'console_scripts': [
